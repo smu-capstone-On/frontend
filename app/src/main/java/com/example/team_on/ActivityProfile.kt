48946ecc8c2b83bfeca648e7efe83319cd0d6d5b
OldJinGyu <chwlsrb00225@naver.com>
2024-05-01 22:20:56 +0900

    feat: 프로필 설정

--- app/src/main/java/com/example/team_on/ActivityProfile.kt
+++ app/src/main/java/com/example/team_on/ActivityProfile.kt
@@ -1,14 +1,97 @@
 package com.example.team_on
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageButton
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.example.team_on.databinding.ActivityProfileBinding
+import de.hdodenhof.circleimageview.CircleImageView
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 class ActivityProfile : AppCompatActivity() {
 
     private val binding: ActivityProfileBinding by lazy { ActivityProfileBinding.inflate(layoutInflater) }
+    private lateinit var changeProfileImage: ImageButton
+    private lateinit var profile: CircleImageView
+    private lateinit var mediaType: MediaType
+
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        if (uri != null) {
+            val path = getRealPathFromUri(uri)
+            val file = File(path)
+            mediaType = "image/*".toMediaType()
+            changeProfile(uri, file)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        changeProfileImage = binding.profileBtnChangeImage
+        profile = binding.profileImageProfile
+
+        changeProfileImage.setOnClickListener{
+            when {
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED -> {
+                    pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                }
+
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                -> {
+                    showPermissionContextPopup()
+                }
+
+                else -> requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1000
+                )
+            }
+        }
+    }
+
+    //이미지 저장 주소 가져오기
+    private fun getRealPathFromUri(uri: Uri): String? {
+        val context = applicationContext
+        val contentResolver = context.contentResolver
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                return it.getString(columnIndex)
+            }
+        }
+        return null
+    }
+
+    //권한 요청
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("프로필 이미지를 설정하기 위해서는 갤러리 접근 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun changeProfile(uri: Uri, file: File){
+        Glide.with(this)
+            .load(uri)
+            .into(profile)
     }
 }
