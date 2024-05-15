6c6415e26cb7a8ac6aca11e195b72ff63cf19f4e
kms146594 <kms146594@gmail.com>
2024-05-15 22:15:04 +0900

    feat: 커뮤니티 생성

--- app/src/main/java/com/example/team_on/ActivityMain.kt
+++ app/src/main/java/com/example/team_on/ActivityMain.kt
@@ -3,19 +3,81 @@ package com.example.team_on
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.team_on.databinding.ActivityMainBinding
 
 class ActivityMain : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var binding: ActivityMainBinding
+
+    private var fragmentHome: FragmentHome? = null
+    private var fragmentCommunity: FragmentCommunity? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
 
-        binding.gotokakao.setOnClickListener {
-            val intent = Intent(this, ActivityWalk::class.java)
-            startActivity(intent)
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        fragmentHome = FragmentHome()
+        supportFragmentManager.beginTransaction().replace(R.id.main_frameLayout, fragmentHome!!).commit()
+
+        binding.mainBottomNavi.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_bottom_home -> {
+                    if (fragmentHome == null) {
+                        fragmentHome = FragmentHome()
+                        supportFragmentManager.beginTransaction().add(R.id.main_frameLayout, fragmentHome!!).commit()
+                    }
+                    if (fragmentHome != null) supportFragmentManager.beginTransaction().show(fragmentHome!!).commit()
+
+                    if (fragmentCommunity != null) supportFragmentManager.beginTransaction().hide(fragmentCommunity!!).commit()
+
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_bottom_walk -> {
+
+                    if (fragmentHome != null) supportFragmentManager.beginTransaction().hide(fragmentHome!!).commit()
+
+                    if (fragmentCommunity != null) supportFragmentManager.beginTransaction().hide(fragmentCommunity!!).commit()
+
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_bottom_community -> {
+                    if (fragmentCommunity == null){
+                        fragmentCommunity = FragmentCommunity()
+                        supportFragmentManager.beginTransaction().add(R.id.main_frameLayout, fragmentCommunity!!).commit()
+                    }
+                    if (fragmentHome != null) supportFragmentManager.beginTransaction().hide(fragmentHome!!).commit()
+
+                    if (fragmentCommunity != null) supportFragmentManager.beginTransaction().show(fragmentCommunity!!).commit()
+
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_bottom_deal -> {
+
+                    if (fragmentHome != null) supportFragmentManager.beginTransaction().hide(fragmentHome!!).commit()
+
+                    if (fragmentCommunity != null) supportFragmentManager.beginTransaction().hide(fragmentCommunity!!).commit()
+
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_bottom_mypage -> {
+
+                    if (fragmentHome != null) supportFragmentManager.beginTransaction().hide(fragmentHome!!).commit()
+
+                    if (fragmentCommunity != null) supportFragmentManager.beginTransaction().hide(fragmentCommunity!!).commit()
+
+                    return@setOnItemSelectedListener true
+                }
+                else ->{
+                    return@setOnItemSelectedListener true
+                }
+            }
         }
     }
 }
